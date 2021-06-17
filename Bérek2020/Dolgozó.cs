5da439495320ad0b69e741e52c16d4f0d381ad8d
zeon77 <zeon77@gmail.com>
2021-06-17 23:23:56 +0200

    2-3. feladat

---
+++ Bérek2020/Dolgozó.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bérek2020
+{
+    class Dolgozó
+    {
+        public string Név { get; set; }
+        public string Nem { get; set; }
+        public string Részleg { get; set; }
+        public int BelépésÉve { get; set; }
+        public int Bér { get; set; }
+
+        public Dolgozó(string sor)
+        {
+            string[] s = sor.Split(';');
+            Név = s[0];
+            Nem = s[1];
+            Részleg = s[2];
+            BelépésÉve = int.Parse(s[3]);
+            Bér = int.Parse(s[4]);
+        }
+    }
+}
--- Bérek2020/Program.cs
+++ Bérek2020/Program.cs
@@ -9,7 +9,13 @@ namespace Bérek2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //2.
+            List<Dolgozó> dolgozok = new List<Dolgozó>();
+            foreach (var sor in File.ReadAllLines("bérek2020.txt").Skip(1))
+                dolgozok.Add(new Dolgozó(sor));
+            
+            //3.
+            Console.WriteLine($"3. feladat: Dolgozók száma: {dolgozok.Count} fő");
         }
     }
 }
